4cd6bcecd9ff1f052848fc67fdc5c10343a976ef
AdityaBinated <86911300+AdityaPatil100@users.noreply.github.com>
2024-12-17 17:11:15 +0530

    update

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dynamic Checkout</title>
-    <script src="https://js.stripe.com/v3/"></script>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Dynamic Checkout</title>
+        <script src="https://js.stripe.com/v3/"></script>
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
 <body class="bg-gray-100 flex justify-center items-center h-screen">
     <div class="bg-white rounded-lg shadow-lg p-6 w-96">
         <div class="text-center mb-6">
