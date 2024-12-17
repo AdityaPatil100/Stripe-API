c838a05d5b3058343657795107b91bc530f6ffa5
AdityaBinated <86911300+AdityaPatil100@users.noreply.github.com>
2024-12-17 16:50:11 +0530

    update

--- resources/views/partials/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<script src="{{ asset('js/jquery.min.js') }}"></script>
-<script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
-<script>
-    $('[data-toggle="tooltip"]').tooltip();
-</script>
